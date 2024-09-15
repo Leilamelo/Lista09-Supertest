27114826c4b60085a297c4046818c93b2ef35992
Leilamelo <leilaamello@gmail.com>
2024-09-14 23:16:09 -0400

    first commit

---
+++ test/api/user.spec.js
@@ -0,0 +1,113 @@
+// Bibliotecas e Framework
+const supertest = require('supertest')
+const userId = 133211201
+const username = 'belaM'
+
+describe('PetStore Swagger - Entidade User', () => {
+
+    // Atributos do grupo/describe
+    const request = supertest('https://petstore.swagger.io/v2') // baseURL
+    const massaUser = require('../../vendors/json/massaUsers')
+
+    it('POST User', async () => {
+
+        // Atributos, Campos, Características, Configurações
+        const user = await require('../../vendors/json/user.json')
+
+        return await request
+            .post('/user')
+            .send(user)
+            .then((res) => {
+                expect(res.statusCode).toBe(200)
+                expect(res.body.code).toEqual(200)
+                expect(res.body.type).toBe('unknown')
+                expect(res.body.message).toBe(userId.toString())
+            })
+    })// Fim do método POST
+
+    // GET
+    it('GET User', async () => {
+        return await request
+            .get(`/user/${username}`)
+            .then((res) => {
+                expect(res.statusCode).toBe(200)
+                expect(res.body.id).toBe(userId)
+                expect(res.body.username).toBe('belaM')
+                expect(res.body.firstName).toBe('Isabela')
+                expect(res.body.lastName).toBe('Melo')
+                expect(res.body.email).toBe('teste2qa@gmail.com')
+                expect(res.body.password).toBe('TST#001')
+                expect(res.body.phone).toBe('027999010203')
+                expect(res.body.userStatus).toBe(1)
+            })
+    })// Fim do método GET
+
+    it('PUT User', async () => {
+        const userUpate = await require('../../vendors/json/userUpate.json')
+        return await request
+            .put(`/user/${username}`)
+            .send(userUpate)
+            .then((res) => {
+                expect(res.statusCode).toBe(200)
+                expect(res.body.code).toEqual(200)
+                expect(res.body.type).toBe('unknown')
+                expect(res.body.message).toBe('133211202')
+            })
+    })//Fim do PUT
+
+    it('DELETE User', async () => {
+        // const username = 'isaM'
+        const user = await require('../../vendors/json/userUpate.json')
+        return await request
+            .delete(`/user/${username}`)
+            .then((res) => {
+                expect(res.statusCode).toBe(200)
+                expect(res.body.code).toEqual(200)
+                expect(res.body.type).toBe('unknown')
+                expect(res.body.message).toBe('belaM')
+            })
+    })
+
+    // Testes Data Driven for Each da massa
+    massaUser.array.forEach(({ id, username, firstName, lastName, email, password, phone, userStatus }) => {
+
+        it(`POST User Data Driven ForEach - ${username}`, () => {
+
+            // Atributos, Campos, Características, Configurações
+            const user = require('../../vendors/json/user.json')
+
+            user.id = id
+            user.username = username
+            user.firstName = firstName
+            user.lastName = lastName
+            user.email = email
+            user.password = password
+            user.phone = phone
+            user.userStatus = userStatus
+
+            // Função de teste
+            return request
+                .post('/user')
+                .send(user)
+                .then((res) => {
+                    expect(res.statusCode).toBe(200)
+                    expect(res.body.code).toEqual(200)
+                    expect(res.body.type).toBe('unknown')
+                    expect(res.body.message).toBe(id.toString())
+                })
+        })// termina o Post
+
+        it(`DELETE User Data Driven ForEach -- ${username}`, () => {
+            return request
+                .delete(`/user/${username}`)
+                .then((res) => {
+                    expect(res.statusCode).toBe(200)
+                    expect(res.body.code).toEqual(200)
+                    expect(res.body.type).toBe('unknown')
+                    expect(res.body.message).toBe(username)
+                })
+        })// termina o DELETE
+
+    }) // termina o For Each da massa
+
+})//Fim describe
---
+++ vendors/json/massaUsers.js
@@ -0,0 +1,32 @@
+module.exports.array = [
+  {
+    "id": 133211203,
+    "username": "joaoS",
+    "firstName": "João",
+    "lastName": "Silva",
+    "email": "joao.silva@gmail.com",
+    "password": "TST#002",
+    "phone": "027999010204",
+    "userStatus": 1
+  },
+  {
+    "id": 133211204,
+    "username": "anaP",
+    "firstName": "Ana",
+    "lastName": "Pereira",
+    "email": "ana.pereira@gmail.com",
+    "password": "TST#003",
+    "phone": "027999010205",
+    "userStatus": 1
+  },
+  {
+    "id": 133211205,
+    "username": "carlosD",
+    "firstName": "Carlos",
+    "lastName": "Dias",
+    "email": "carlos.dias@gmail.com",
+    "password": "TST#004",
+    "phone": "027999010206",
+    "userStatus": 1
+  }
+]
